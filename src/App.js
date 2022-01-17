a081963ede870617168502a0567a8faf0aea4421
Ricardo <rlevenhagen@gmail.com>
2022-01-17 12:47:16 -0500

    Small changes on the main

--- src/App.js
+++ src/App.js
@@ -38,10 +38,11 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Welcome back to the Art Museum</h1>
+      <h1> Welcome back to the Art Museum </h1>
+      <h2> Testing by climbing using React with git </h2>
 
       <form onSubmit={ handleSubmit }>
-        <label htmlFor="search">Search for Art:</label>
+        <label htmlFor="search">  Search for Art:  </label>
         <input type="text" id="search" onChange={ handleInput } value={userInput}/>
         <button>Search</button>
       </form>

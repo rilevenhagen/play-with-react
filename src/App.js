fd32460321add4691ec883577a87d516984e0057
Ricardo <rlevenhagen@gmail.com>
2022-01-17 12:04:55 -0500

    let see how it goeswith react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const apiKey = '7WY91ct7';
+
+  const [art, setArt] = useState([]);
+  const [userInput, setUserInput] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  // Make an API to the Rijks Museum API
+  useEffect(() => {
+    axios({
+      url: 'https://www.rijksmuseum.nl/api/en/collection',
+      method: 'GET',
+      dataResponse: 'json',
+      params: {
+        key: apiKey,
+        imgonly: true,
+        q: searchTerm,
+      }
+    }).then((response) => {
+      console.log(response.data.artObjects);
+      setArt(response.data.artObjects);
+    });
+  }, [searchTerm])
+
+  const handleInput = (event) => {
+    console.log('is this working?', event.target.value);
+    setUserInput(event.target.value); 
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearchTerm(userInput);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome back to the Art Museum</h1>
+
+      <form onSubmit={ handleSubmit }>
+        <label htmlFor="search">Search for Art:</label>
+        <input type="text" id="search" onChange={ handleInput } value={userInput}/>
+        <button>Search</button>
+      </form>
+
+      {art.map((artwork) => {
+        return (
+          <div key={artwork.id}>
+            <h2>{artwork.longTitle}</h2>
+            <img src={artwork.webImage.url} alt={artwork.title} />
+          </div>
+        )
+      })}
     </div>
   );
 }
